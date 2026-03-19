9395756db5e226d0c8159cbfbf645acfb902c9f2
박현규 <phg930421@softzion.com>
2026-03-19 17:04:55 +0900

    fix: getTmdbImageUrl 수정

--- src/lib/tmdb-image.ts
+++ src/lib/tmdb-image.ts
@@ -1,6 +1,8 @@
+export type TmdbImageSize = 'w500' | 'w780' | 'w1280' | 'original';
+
 export function getTmdbImageUrl(
   path: string,
-  size: 'w500' | 'original' = 'w500'
+  size: TmdbImageSize = 'w500'
 ) {
   return `https://image.tmdb.org/t/p/${size}${path}`;
 }
